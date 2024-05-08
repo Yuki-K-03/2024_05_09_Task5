b8ad8f5e2f056f8587a5d15ca3a36b9ed1fd7f63
PC-Ow <PC-Ow@PC-OWNER>
2024-05-09 03:56:29 +0900

    プロジェクト ファイルを追加します。

---
+++ 2024_05_09_Task5/2024_05_09_Task5.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+
+template <typename TYPE>
+void Show(const vector<TYPE> v) {
+    for (int i = 0; i < v.size(); i++) {
+        cout << v[i] << " " << flush;
+    }
+    cout << endl;
+}
+
+int main()
+{
+    vector<int> v(5, 0);
+
+    for (int i = 0; i < v.size(); i++) {
+        v[i] = i + 1;
+    }
+
+    Show(v);
+
+
+    v.resize(10, 0);
+
+    for (int i = 0; i < v.size(); i++) {
+        v[i] += 1;
+    }
+
+    Show(v);
+
+
+    return 0;
+}
